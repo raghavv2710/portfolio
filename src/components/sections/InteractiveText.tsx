08bd78ce7fa45867772ea4315727c81810dcb025
Raghavendra SR <raghavv2710@gmail.com>
2025-07-11 18:11:06 +0000

    the higlight text is not suitable for mobile devices change the way the

--- src/components/sections/InteractiveText.tsx
+++ src/components/sections/InteractiveText.tsx
@@ -1,5 +1,36 @@
+'use client';
+import { useIsMobile } from "@/hooks/use-mobile";
+import { useEffect, useState } from "react";
+
 const InteractiveText = () => {
   const text = "Hi, I am Raghavendra";
+  const [isClient, setIsClient] = useState(false);
+  
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  const isMobile = useIsMobile();
+  
+  if (!isClient) {
+    return (
+      <div className="h-auto min-h-[90px] flex items-center justify-center">
+        <h2 className="static-heading">
+          Hi, I am <span className="name-highlight">Raghavendra</span>
+        </h2>
+      </div>
+    );
+  }
+
+  if (isMobile) {
+    return (
+      <div className="h-auto min-h-[90px] flex items-center justify-center">
+        <h2 className="static-heading">
+          Hi, I am <span className="name-highlight">Raghavendra</span>
+        </h2>
+      </div>
+    );
+  }
 
   return (
     <div className="canyon-word flex flex-wrap justify-center items-center gap-[4px]">
